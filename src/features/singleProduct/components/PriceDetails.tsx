68627819b2ac1c3994c64f7a33b6d0f7a3a6335f
Evgeny Kovalev <e.kovalev0709@gmail.com>
2024-04-01 17:37:20 +0300

    create and use svg icons

--- src/features/singleProduct/components/PriceDetails.tsx
+++ src/features/singleProduct/components/PriceDetails.tsx
@@ -1,28 +1,21 @@
+import { DoorCasingIcon, DoorFrameIcon, DoorIcon } from '@/shared/icons';
+
 interface PriceDetailsProps {}
 
 export const PriceDetails = ({}: PriceDetailsProps) => {
 	return (
 		<div className="flex flex-wrap items-center justify-between gap-2 text-muted-foreground">
 			<figure className="flex items-center">
-				{/* //!FIX: create icons   */}
-				<div className="mr-3 h-10 w-10 rounded-lg bg-primary"></div>
-				<figcaption>
-					<span>Полотно</span>
-				</figcaption>
+				<DoorIcon className="mr-2 h-10 w-10 text-primary" />
+				<figcaption>Полотно</figcaption>
 			</figure>
 			<figure className="flex items-center">
-				{/* //!FIX: create icons   */}
-				<div className="mr-3 h-10 w-10 rounded-lg bg-primary"></div>
-				<figcaption>
-					<span>Коробка (2.5шт)</span>
-				</figcaption>
+				<DoorFrameIcon className="mr-2 h-10 w-10 text-primary" />
+				<figcaption>Коробка (2.5шт)</figcaption>
 			</figure>
 			<figure className="flex items-center">
-				{/* //!FIX: create icons   */}
-				<div className="mr-3 h-10 w-10 rounded-lg bg-primary"></div>
-				<figcaption>
-					<span>Наличник (5шт)</span>
-				</figcaption>
+				<DoorCasingIcon className="mr-2 h-10 w-10 text-primary" />
+				<figcaption>Наличник (5шт)</figcaption>
 			</figure>
 		</div>
 	);
--- src/widgets/Header/components/Logo.tsx
+++ src/widgets/Header/components/Logo.tsx
@@ -1,3 +1,4 @@
+import { LogoIcon } from '@/shared/icons';
 import { cn } from '@/shared/ui/utils';
 import Link from 'next/link';
 
@@ -6,15 +7,15 @@ interface LogoProps extends React.HTMLAttributes<HTMLDivElement> {}
 export default function Logo({ className }: LogoProps) {
 	return (
 		<div className={cn(className)}>
-			<Link href={'/'} className={cn('flex items-center')}>
+			<Link href={'/'} className={cn('inline-flex items-center')}>
 				<div className="mr-3">
-					<div className="h-10 w-10 rounded-full bg-primary sm:h-12 sm:w-12"></div>
+					<LogoIcon className="h-10 text-primary sm:h-12" />
 				</div>
 				<div className="flex flex-col">
-					<div className="flex items-center text-base font-bold leading-tight after:ml-1 after:inline-block after:h-2 after:w-2 after:rounded-full after:bg-primary after:content-[''] sm:text-lg">
+					<div className="flex items-center text-lg font-bold leading-tight after:ml-1 after:inline-block after:h-2 after:w-2 after:rounded-full after:bg-primary after:content-[''] sm:text-lg md:text-xl lg:text-2xl">
 						ДВЕРИ
 					</div>
-					<div className="text-nowrap text-xs leading-tight sm:text-sm md:text-base">
+					<div className="text-nowrap  text-base leading-tight lg:text-lg">
 						Входные и Межкомнатные
 					</div>
 				</div>
