00623b1d94a1425ed80b297d63f541b4234aa743
Evgeny Kovalev <e.kovalev0709@gmail.com>
2024-05-29 15:30:44 +0300

    rename env vars

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: 'http',
-				hostname: process.env.API_HOSTNAME,
-				port: process.env.API_PORT,
-				pathname: process.env.API_PATHNAME,
+				hostname: process.env.IMAGES_HOSTNAME,
+				port: process.env.IMAGES_PORT,
+				pathname: process.env.IMAGES_PATHNAME,
 			},
 		],
 	},
