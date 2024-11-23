7b27eb3b94805c05a13ec3d0907f61c9ce1e53e3
Evgeny Kovalev <e.kovalev0709@gmail.com>
2024-11-23 22:52:51 +0300

    style: change ProductCards grid

--- src/widgets/products/ProductCards/ui/ProductCards.tsx
+++ src/widgets/products/ProductCards/ui/ProductCards.tsx
@@ -7,7 +7,7 @@ interface ProductListProps {
 
 export const ProductCards = ({ products }: ProductListProps) => {
 	return (
-		<ul className="grid grid-cols-2 gap-5 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+		<ul className="grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
 			{products.map((product) => {
 				return (
 					<li
