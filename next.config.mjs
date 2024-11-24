fbf0e91144d70fca003df21b617a3cf319363ddc
Evgeny Kovalev <e.kovalev0709@gmail.com>
2024-11-24 21:21:12 +0300

    fix: error in next config during linting

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: 'http',
-				hostname: process.env.IMAGES_HOSTNAME,
+				hostname: process.env.IMAGES_HOSTNAME ?? '',
 				port: process.env.IMAGES_PORT,
-				pathname: process.env.IMAGES_PATHNAME,
+				pathname: process.env.IMAGES_PATHNAME ?? '',
 			},
 		],
 	},
