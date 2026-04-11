dfc1f02d59d7b535e3061daa3f81bd27c2b58ab4
Evgeny-Kovalev <e.kovalev0709@gmail.com>
2026-04-11 22:28:22 +0300

    fix: product card favorite button with loader issue

--- src/entities/product/ui/ProductCard.tsx
+++ src/entities/product/ui/ProductCard.tsx
@@ -20,60 +20,64 @@ export const ProductCard = ({ product, className, ...props }: ProductCardProps) 
 	}, []);
 
 	return (
-		<Link href={'/products/' + product.slug}>
-			<Card className={cn('rounded-t-none', className)} {...props}>
-				<CardContent className="relative p-0 ">
-					<div className="absolute left-1 top-2 flex flex-col gap-1">
-						{tags.map((t) => (
-							<Badge
-								key={t.id}
-								className={cn('w-fit', {
-									'bg-red-500 hover:bg-red-500': t.key === TagKeys.bestseller,
-									'bg-green-500 hover:bg-green-500 ': t.key === TagKeys.new,
-									'bg-amber-500 hover:bg-amber-500': t.key === TagKeys.sample,
-								})}
-							>
-								{t.label}
-							</Badge>
-						))}
-					</div>
-					<Image
-						src={product.imgUrl}
-						className="h-auto w-full"
-						sizes="100vw"
-						width={600}
-						height={product.category.categoryType === 'interiorDoors' ? 1200 : 600}
-						alt="Product image"
-						placeholder="blur"
-						blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8+vx1PQAIqAM4jZDFJQAAAABJRU5ErkJggg=="
-						priority
-					/>
-					<FavoriteToggleButton
-						className="absolute right-2 top-2"
-						product={product}
-					/>
-				</CardContent>
-				<CardFooter className="p-2 ">
-					<div className="mx-auto flex w-full flex-col items-center text-center">
-						<CardTitle className="text-sm hover:text-primary-accent sm:text-base">
-							{product.name}
-						</CardTitle>
-						<div className="text-sm font-bold text-primary-accent sm:text-base">
-							{priceText}
+		<div className="relative">
+			<FavoriteToggleButton
+				className="absolute top-2 right-2 z-10"
+				product={product}
+			/>
+			<Link href={'/products/' + product.slug}>
+				<Card className={cn('rounded-t-none', className)} {...props}>
+					<CardContent className="relative p-0">
+						<div className="absolute top-2 left-1 flex flex-col gap-1">
+							{tags.map((t) => (
+								<Badge
+									key={t.id}
+									className={cn('w-fit', {
+										'bg-red-500 hover:bg-red-500': t.key === TagKeys.bestseller,
+										'bg-green-500 hover:bg-green-500': t.key === TagKeys.new,
+										'bg-amber-500 hover:bg-amber-500': t.key === TagKeys.sample,
+									})}
+								>
+									{t.label}
+								</Badge>
+							))}
 						</div>
-						{product.category.categoryType === 'interiorDoors' && (
-							<div className="text-sm font-bold sm:text-base">
-								{product.productType === 'doorOnlySample'
-									? 'полотно'
-									: 'за комплект'}
+						<Image
+							src={product.imgUrl}
+							className="h-auto w-full"
+							sizes="100vw"
+							width={600}
+							height={
+								product.category.categoryType === 'interiorDoors' ? 1200 : 600
+							}
+							alt="Product image"
+							placeholder="blur"
+							blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8+vx1PQAIqAM4jZDFJQAAAABJRU5ErkJggg=="
+							priority
+						/>
+					</CardContent>
+					<CardFooter className="p-2">
+						<div className="mx-auto flex w-full flex-col items-center text-center">
+							<CardTitle className="hover:text-primary-accent text-sm sm:text-base">
+								{product.name}
+							</CardTitle>
+							<div className="text-primary-accent text-sm font-bold sm:text-base">
+								{priceText}
 							</div>
-						)}
-						<Button className="mt-2 w-full" size="sm">
-							Подробнее
-						</Button>
-					</div>
-				</CardFooter>
-			</Card>
-		</Link>
+							{product.category.categoryType === 'interiorDoors' && (
+								<div className="text-sm font-bold sm:text-base">
+									{product.productType === 'doorOnlySample'
+										? 'полотно'
+										: 'за комплект'}
+								</div>
+							)}
+							<Button className="mt-2 w-full" size="sm">
+								Подробнее
+							</Button>
+						</div>
+					</CardFooter>
+				</Card>
+			</Link>
+		</div>
 	);
 };
