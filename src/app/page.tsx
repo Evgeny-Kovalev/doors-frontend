43f157a230552a73f9780877c01c64c1b06043d1
Evgeny-Kovalev <e.kovalev0709@gmail.com>
2026-04-19 18:45:53 +0300

    fix: missing prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default async function Home() {
 				<section>
 					<div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
 						{favoriteCategories.categories.map((cat) => (
-							<CategoryCard key={cat.id} category={cat} />
+							<CategoryCard key={cat.id} category={cat}  orientation='horizontal'/>
 						))}
 					</div>
 				</section>
